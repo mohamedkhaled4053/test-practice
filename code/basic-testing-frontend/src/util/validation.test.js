26bccfd2d93937454813fb54b166bfd587ef2ee5
mohamedkhaled4053 <mohamedkhaled4053@gmail.com>
2022-10-31 13:22:55 +0200

    use suites to organize tests

--- code/basic-testing-frontend/src/util/validation.test.js
+++ code/basic-testing-frontend/src/util/validation.test.js
@@ -1,38 +1,41 @@
-import { expect, it } from "vitest";
-import { validateNumber, validateStringNotEmpty } from "./validation";
+import { describe, expect, it } from 'vitest';
+import { validateNumber, validateStringNotEmpty } from './validation';
 
+describe('validateStringNotEmpty()', () => {
+  it('should throw error if input is empty', () => {
+    let arg = '';
+    let arg2 = '     ';
 
-it('should throw error if input is empty',()=>{
-    let arg = ''
-    let arg2 = '     '
+    let resultFn = () => {
+      validateStringNotEmpty(arg);
+    };
+    let resultFn2 = () => {
+      validateStringNotEmpty(arg2);
+    };
 
-    let resultFn = ()=>{
-        validateStringNotEmpty(arg)
-    }
-    let resultFn2 = ()=>{
-        validateStringNotEmpty(arg2)
-    }
+    expect(resultFn).toThrow(/Invalid input/);
+    expect(resultFn2).toThrow(/Invalid input/);
+  });
 
-    expect(resultFn).toThrow(/Invalid input/)
-    expect(resultFn2).toThrow(/Invalid input/)
-})
+  it('should throw error if argument is not string', () => {
+    let arg = [];
 
-it('should throw error if argument is not string',()=>{
-    let arg = []
+    let resultFn = () => {
+      validateStringNotEmpty(arg);
+    };
 
-    let resultFn = ()=>{
-        validateStringNotEmpty(arg)
-    }
+    expect(resultFn).toThrow(/Invalid input/);
+  });
+});
 
-    expect(resultFn).toThrow(/Invalid input/)
-})
+describe('validateNumber', () => {
+  it('should throw error if argument is not valid', () => {
+    let arg = '1';
 
-it('should throw error if argument is not valid',()=>{
-    let arg = '1'
+    let resultFn = () => {
+      validateNumber(arg);
+    };
 
-    let resultFn = ()=>{
-        validateNumber(arg)
-    }
-
-    expect(resultFn).toThrow(/Invalid number/)
-})
+    expect(resultFn).toThrow(/Invalid number/);
+  });
+});
