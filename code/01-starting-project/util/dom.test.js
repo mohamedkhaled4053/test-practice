e545d666efa0cb23524e14c59488c01dedc8ede1
mohamedkhaled4053 <mohamedkhaled4053@gmail.com>
2022-11-15 10:50:06 +0200

    Setting up a Virtual HTML Page

--- code/01-starting-project/util/dom.test.js
+++ code/01-starting-project/util/dom.test.js
@@ -1,5 +1,17 @@
-import {it} from 'vitest'
+import {it, vi} from 'vitest'
 import { showError } from './dom'
+import path from "path";
+import fs from "fs";
+import { Window } from "happy-dom";
+
+let htmlDocPath = path.join(process.cwd(), 'index.html')
+let htmlDocContent = fs.readFileSync(htmlDocPath).toString()
+
+let window = new Window()
+let document = window.document
+document.write(htmlDocContent)
+
+vi.stubGlobal('document',document)
 
 it('should',()=>{
     showError('testMessage')
